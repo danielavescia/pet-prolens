b22cd5fd997efac639b39db02b4da3ec0a469a78
Daniela Vescia <danivescia@gmail.com>
2023-09-19 09:28:38 -0300

    bug sobre.js & sobre.css fixed

--- OneDrive/Área de Trabalho/React-master/React-master/src/App.js
+++ OneDrive/Área de Trabalho/React-master/React-master/src/App.js
@@ -1,6 +1,7 @@
 import Portfolio from "./components/Portfolio.js";
 import Home from "./components/Home.js";
 import Navbar from "./components/Navbar.js";
+import Sobre from "./components/Sobre.js";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import "./styles/index.css";
@@ -14,6 +15,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/sobre" element={<Sobre />} />
           </Routes>
         </Router>
       </div>
